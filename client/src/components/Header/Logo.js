c0095fcb7feecf18ee772eb686845efa79975ed4
onely6721 <52649403+onely6721@users.noreply.github.com>
2021-12-16 00:45:59 +0200

    updated

--- client/src/components/Header/Logo.js
+++ client/src/components/Header/Logo.js
@@ -2,19 +2,11 @@ import {Typography} from "@mui/material";
 import {Link} from "react-router-dom";
 import {makeStyles} from "@mui/styles";
 
-const useStyles = makeStyles({
-  logo: {
-      '@media (max-width: 500px)': {
-          display: "none"
-      }
-  }
-});
+
 
 export const Logo = () => {
-    const classes = useStyles()
     return (
         <Typography
-            className={classes.logo}
             variant="h6"
             component="div"
             sx={{ flexGrow: 1, color:"white"}}
@@ -23,7 +15,7 @@ export const Logo = () => {
                 to="/"
                 style={{color: "white"}}
             >
-                Multiplex Clone
+                MXC
             </Link>
         </Typography>
     )
